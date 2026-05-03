4249c7cce798111f338241272727428c61221496
SearchBoxStrategies <adrien@searchboxstrategies.com>
2026-05-03 18:32:10 -0300

    Update Billing.tsx

--- client/src/pages/Billing.tsx
+++ client/src/pages/Billing.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { api } from '../lib/api';
+import api from '../lib/api';
 import { useAuth } from '../context/AuthContext';
 
 export default function Billing() {
