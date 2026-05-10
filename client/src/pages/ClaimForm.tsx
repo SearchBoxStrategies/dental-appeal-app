d5ec553fffaa80d14c50ae381e11986938e359a2
SearchBoxStrategies <adrien@searchboxstrategies.com>
2026-05-10 07:36:45 -0300

    Update ClaimForm.tsx

--- client/src/pages/ClaimForm.tsx
+++ client/src/pages/ClaimForm.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import CDTSelector from './CDTSelector';
+import CDTSelector from '../components/CDTSelector';
 import api from '../lib/api';
 
 interface InsuranceCompany {
